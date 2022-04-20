e971d8ff03d011bfc374f6914da0e452417f7150
StanleyChanRvt <95653986+StanleyChanRvt@users.noreply.github.com>
2022-04-20 14:36:32 -0500

    Add files via upload
    Build Order: You are given a list of projects and a list of dependencies (which is a list of pairs of projects, where the second project is dependent on the first project). All of a project's dependencies must be built before the project is. Find a build order that will allow the projects to be built. If there is no valid build order, return an error.
    Example:
    Input:
    projects: a, b, c, d, e, f
    dependencies: (a, d), (f, b), (b, d), (f, a), (d, c)
    Output: f, e, a, b, d, c

---
+++ BuildOrder/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace BuildOrder
+{
+    class Program
+    {
+        public static string[] BuildOrderSolve(string[] inputProjects, Hashtable dependencies)
+        {
+            string[] solution = new string[inputProjects.Length];
+            string order = inputProjects[0];
+            int index = 0;
+ 
+
+            foreach(string project in inputProjects)
+            {
+                if(!dependencies.ContainsValue(project))
+                {
+                    solution[index] = project;
+                    index++;
+                }
+            }//find projects that are independent
+
+            for(int ValueKeys = 0; ValueKeys < inputProjects.Length; ValueKeys++)
+            {
+                if(dependencies.ContainsKey(inputProjects[ValueKeys]) && dependencies.ContainsValue(inputProjects[ValueKeys]))
+                {
+                    solution[index] = inputProjects[ValueKeys];
+                    index++;
+                }
+            }//find projects that are both depend on project and projects depend on them
+
+            for(int onlyValues = 0; onlyValues < inputProjects.Length; onlyValues++)
+            {
+                if (!dependencies.ContainsKey(inputProjects[onlyValues]) && dependencies.ContainsValue(inputProjects[onlyValues]))
+                {
+                    solution[index] = inputProjects[onlyValues];
+                    index++;
+                }
+            }//find projects that only depend on other projects
+
+
+            if (index != inputProjects.Length+1)
+                Console.WriteLine("valid build order not found");
+            return solution;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] projects = new string[6] {"a","b", "c", "d", "e", "f" };
+            var dependentPairs = new Hashtable()
+            {
+                {"a", "d"},
+                {"f", "b"},
+                {"b", "d"},
+                {"f", "a"},
+                {"d", "c"}
+            };
+
+            projects = BuildOrderSolve(projects, dependentPairs);
+        }
+    }
+}
